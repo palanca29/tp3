66033dfb7d065e093fe7b19f75db470ed074ebaf
palanca29 <jcerrato@lasalleflorida.edu.ar>
2024-06-27 13:19:57 -0300

    Conexion TCP

--- client.py
+++ client.py
@@ -1,10 +1,7 @@
 import socket
 
-s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-s.connect(('localhost', 12345))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 12345))
 s.send("Hola, servidor".encode())
 response = s.recv(1024).decode()
-
 print(response)
-
--- server.py
+++ server.py
@@ -1,15 +1,11 @@
 import socket
 
-s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost',12345))
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 12345))
 s.listen()
 print("esperando conexion")
 conn, addr = s.accept()
-
-data = conn.recv(1024)   
-
+data = conn.recv(1024)
 conn.sendall("Message for client".encode())
+print(data.decode())
 
-print(data)
-#ola
