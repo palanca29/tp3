ab5328e8162e461e2b96609b1ea45f95d832b2b3
palanca29 <jcerrato@lasalleflorida.edu.ar>
2024-07-01 12:35:28 -0300

    Server conexion 2 pcs

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('127.0.0.1', 12345))
-print("esperando mensaje")
-data, addr = s.recvfrom(1024)
-s.sendto("Message for client".encode(), addr)
-print(data.decode())
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('192.2.6.106',12345))
 
+s.listen()
+print("esperando conexion")
+conn, addr = s.accept()
 
+data = conn.recv(1024)   
+
+conn.sendall("Message for client".encode())
+
+print(data)
